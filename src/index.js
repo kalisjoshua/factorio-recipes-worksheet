10c483eb7365c69eff77b4a5d9595703743d81c1
Joshua T Kalis <kalisjoshua@gmail.com>
2017-12-06 19:58:25 -0500

    add missing pieces from project set up

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import newStore from "./storage"
 const store = newStore("2017-11-20")
 const root = document.getElementById("root")
 
-if (root && typeof window !== "undefined") {
-  function render(state) {
-    preactRender(<App {...{store, render, state}} />, root, root.lastChild)
-  }
-
-  render()
+function render(state) {
+  preactRender(<App {...{store, render, state}} />, root, root.lastChild)
 }
+
+root && typeof window !== "undefined" && render()
